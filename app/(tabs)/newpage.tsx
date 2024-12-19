391865384ce0b4d268ab246847d0ec4d0d011e37
MOHAMMED LABEEB A P <labeebap446@gmail.com>
2024-12-19 11:27:04 +0530

    initial commit

---
+++ app/(tabs)/newpage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const newpage = () => {
+  return (
+    <View>
+      <Text>newpage</Text>
+    </View>
+  )
+}
+
+export default newpage
