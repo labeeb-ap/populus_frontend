ed54dc37dd704e05a3124f8e8d0967e6749ac532
niss-shana <134854766+niss-shana@users.noreply.github.com>
2024-12-25 06:44:45 +0530

    cvc

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="contact"
         options={{
-          title: 'Home',
+          title: 'contact',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
